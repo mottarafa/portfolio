d8fdb7135436eb9d4ff87eaad3084ac6cbafe718
mottarafa <rafaelmotta18@gmail.com>
2022-07-19 15:26:00 -0300

    Final

--- static/script.js
+++ static/script.js
@@ -3,10 +3,10 @@
 const menuMobile = document.querySelector('.menu-mobile')
 const body = document.querySelector('body')
 
-menuMobile.addEventListener('click', () =>{
+menuMobile.addEventListener('click', () => {
     menuMobile.classList.contains("bi-list")
         ? menuMobile.classList.replace("bi-list", "bi-x")
-        : menuMobile.classList.replace("bi-x", "bi-list");
+        : menuMobile.classList.replace("bi-x", "bi-list")
     body.classList.toggle("menu-nav-active");
 });
 
@@ -14,11 +14,32 @@ menuMobile.addEventListener('click', () =>{
 
 const navItem = document.querySelectorAll('.nav-item')
 
-navItem.forEach(item =>{
+navItem.forEach(item => {
     item.addEventListener("click", () => {
         if (body.classList.contains("menu-nav-active")){
             body.classList.remove("menu-nav-active")
             menuMobile.classList.replace("bi-x", "bi-list")
         }
     })
+})
+
+// Animar todos os itens na tela que tiverem meu atributo data-anime
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
+    
+    item.forEach((element) => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add("animate");
+        } else {
+            element.classList.remove("animate")
+        }
+    });
+};
+
+animeScroll();
+window.addEventListener("scroll", ()=>{
+    animeScroll();
 })
